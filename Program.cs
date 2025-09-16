29ef6b51afa378cce46021f50453fa1f0f6efc38
Sohila179 <Sohilabendary674@gmail.com>
2025-09-16 19:54:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Immutable;
+using System.Numerics;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibraryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
