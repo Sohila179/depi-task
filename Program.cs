44ebd0b3a973cc344c6bee10402d05fa3814214a
Sohila179 <Sohilabendary674@gmail.com>
2025-09-16 20:04:31 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CompanyEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Immutable;
+using System.Numerics;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace CompanyEFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CompanyT1Context c = new CompanyT1Context();
+            var res = c.Departments.ToList();
+            foreach(var item in res)
+            {
+                Console.WriteLine(item.Dname);
+            }
+        }
+    }
+}
